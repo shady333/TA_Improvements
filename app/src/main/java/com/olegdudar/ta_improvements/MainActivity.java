4f7345d5e3c0cf4979e89c14d79a070025584943
Oleg Dudar <oleg_dudar@epam.com>
2016-06-29 16:20:25 +0300

    Added some activity

--- app/src/main/java/com/olegdudar/ta_improvements/MainActivity.java
+++ app/src/main/java/com/olegdudar/ta_improvements/MainActivity.java
@@ -1,13 +1,26 @@
 package com.olegdudar.ta_improvements;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = (Button)findViewById(R.id.btn_start);
+//        startButton.setOnClickListener(goToApplicationTypeSelection(this));
+    }
+
+    public void goToApplicationTypeSelection(View view){
+        Intent intent = new Intent(this, TypeOfApplication.class);
+        startActivity(intent);
     }
 }
